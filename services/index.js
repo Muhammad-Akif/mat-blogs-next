6540f77203f2fc8b74f1818427cc5a5a853d4d95
muhammad-akif <muhammadakif2917@gmail.com>
2021-11-28 23:06:28 +0500

    Categories Comp Completed

--- services/index.js
+++ services/index.js
@@ -84,7 +84,7 @@ export const getSimilarPosts = async () => {
 export const getCategories = async () => {
   const query = gql`
     query GetCategories {
-      categories: { 
+      categories { 
         name
         slug
       }
