efa510dab28e93c70176f20c3a5cf2dd669f1e66
muhammad-akif <muhammadakif2917@gmail.com>
2021-11-28 21:47:26 +0500

    added few line in PostWidget

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import moment from 'moment';
+import Link from 'next/link'
+import { getRecentPosts, getSimilarPosts } from '../services'
 
 const PostWidget = () => {
+    const [relatedPosts, setRelatedPosts] = useState([]);
+    useEffect(() => {
+        if (slug) {
+            getSimilarPosts(category, slug).then((result) => setRelatedPosts(result))
+        }
+        else
+            getRecentPosts(category, slug).then((result) => setRelatedPosts(result))
+    }, [])
     return (
         <div>
             PostWidget
