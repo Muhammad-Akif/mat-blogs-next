0d228897dd0875009b79b0c0cffa5eeb657c4c79
muhammad-akif <muhammadakif2917@gmail.com>
2021-11-29 21:59:44 +0500

    added PostDetails Child Components

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { getPosts, getPostDetails } from '../../services'
+import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm } from '../../components'
 
 const PostDetails = () => {
     return (
-        <div>
-            
+        <div className="container mx-auto px-10 mb-8">
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+                <div className="lg:col-span-8 col-span-1">
+                    <PostDetail />
+                    <Author />
+                    <CommentsForm />
+                    <Comments />
+                </div>
+                <div className="lg:col-span-4 col-span-1">
+                    <div className="lg:sticky relative top-8">
+                        <PostWidget />
+                        <Categories />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
