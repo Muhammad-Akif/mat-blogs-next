7c59b87e41d4d91a27a350aff13171d1ea617d7a
muhammad-akif <muhammadakif2917@gmail.com>
2021-12-02 22:43:36 +0500

    updated CommentForm component

--- components/Author.jsx
+++ components/Author.jsx
@@ -14,8 +14,8 @@ const Author = ({ author }) => {
                     src={author.photo.url}
                 />
             </div>
-                <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
-                <p className="text-white text-lg">{author.bio}</p>
+            <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+            <p className="text-white text-lg">{author.bio}</p>
         </div>
     )
 }
--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -68,10 +68,10 @@ const PostDetail = ({ post }) => {
                     </div>
                     <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
                     {
-                        console.log(post.content.raw.children.map((typeObj, index) => {
+                        post.content.raw.children.map((typeObj, index) => {
                             const children = typeObj.children.map((item, itemindex) => getContentFragment(itemindex, item.text, item));
                             return getContentFragment(index, children, typeObj, typeObj.type);
-                        }))
+                        })
                     }
                 </div>
             </div>
