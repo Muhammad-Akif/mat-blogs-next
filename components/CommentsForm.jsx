7a13559f5e13c15a65fa4b9871939dcec94f666a
muhammad-akif <muhammadakif2917@gmail.com>
2021-12-03 21:42:52 +0500

    added few hooks in commentform

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState, useRef} from 'react'
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
+    const [error, setError] = useState(false);
+    const [localstorage, setLocalstorage] = useState(null);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const commentEl = useRef();
+    const nameEl = useRef();
+    const emailEl = useRef();
+    const storeDataEl = useRef();
+    
     return (
         <div>
             <h1>CommentsForm</h1>
--- services/index.js
+++ services/index.js
@@ -130,3 +130,4 @@ export const getCategories = async () => {
 
   return result.categories;
 }
+
