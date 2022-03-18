5849ebd9dbe575d3debe64f1c011a2a23e99da81
muhammad-akif <muhammadakif2917@gmail.com>
2022-03-18 14:40:53 +0500

    updated v:1.0.1

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
                 <div className="md:float-left">
                     <Link href="/">
                         <span className="cursor-pointer font-bold text-4xl text-white">
-                            GraphCMS
+                            MatBlogs
                         </span>
                     </Link>
                 </div>
